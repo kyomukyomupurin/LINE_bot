023529eb997ab1a6efe81063b1689cb08a371113
kyomukyomupurin <murakami1728@gmail.com>
2020-08-31 14:31:51 +0900

    :sparkles: Update

--- contest_info.py
+++ contest_info.py
@@ -4,42 +4,46 @@ import datetime
 import time
 
 
-url = 'https://codeforces.com/api/contest.list'
-r = requests.get(url)
-r_json = r.json()
+contest_url = 'https://codeforces.com/api/contest.list'
+line_url = 'https://notify-api.line.me/api/notify'
 num_future_contest = 10
 
-messages = []
 
-for info in r_json['result'][0:num_future_contest]:
-    if (info["phase"] == "BEFORE"):
-        start_unix_time = info["startTimeSeconds"]
-        start_time = datetime.datetime.fromtimestamp(start_unix_time)
-        time_before_start = -info["relativeTimeSeconds"]
-        day_before_start = time_before_start // (60 * 60 * 24)
-        time_before_start %= (60 * 60 * 24)
-        hour_before_start = time_before_start // (60 * 60)
-        time_before_start %= (60 * 60)
-        minutes_before_start = time_before_start // 60
-        message = info["name"] + " start at " + \
-            str(start_time) + ", in " + \
-            str(day_before_start) + " days " + \
-            str(hour_before_start) + " hours " + \
-            str(minutes_before_start) + " minutes."
-        messages.append(message)
+def convert_time(timestamp : int) -> str:
+    day = timestamp // (60 * 60 * 24)
+    s1 = ' day ' if day == 1 else ' days '
+    timestamp %= (60 * 60 * 24)
+    hour = timestamp // (60 * 60)
+    s2 = ' hour ' if hour == 1 else ' hours '
+    timestamp %= (60 * 60)
+    minute = timestamp // 60
+    s3 = ' minute.' if minute == 1 else ' minutes.'
 
-messages.reverse()
+    return str(day) + s1 + str(hour) + s2 + str(minute) + s3
 
-secret_token = ""
 
-with open('./token.txt', 'r') as f:
-    secret_token = f.read()
+if __name__ == '__main__':
+    r_json = requests.get(contest_url).json()
+    messages = []
 
-line_url = "https://notify-api.line.me/api/notify"
-token = secret_token
-headers = {"Authorization": "Bearer " + token}
+    for contest in r_json['result'][0:num_future_contest]:
+        if (contest['phase'] == 'BEFORE'):
+            start_unix_time = contest['startTimeSeconds']
+            start_time = datetime.datetime.fromtimestamp(start_unix_time)
+            time_before_start = -contest['relativeTimeSeconds']
+            message = contest['name'] + ' start at ' + str(start_time) + ', in ' + convert_time(time_before_start)
+            messages.append(message)
 
-for contest in messages:
-    payload = {"message": message}
-    res = requests.post(line_url, headers=headers, params=payload)
-    time.sleep(3)
+    messages.reverse()
+
+    line_token = ''
+
+    with open('./token.txt', 'r') as f:
+        line_token = f.read()
+
+    headers = {'Authorization': 'Bearer ' + line_token}
+
+    for contest in messages:
+        payload = {'message': message}
+        res = requests.post(line_url, headers=headers, params=payload)
+        time.sleep(3)
